d3f392ed5c8ca628379abc7b50bcf6dee61e05fd
lh6032 <lh6032@cs.rit.edu>
2021-08-10 21:03:15 -0400

    outlier range

--- src/component/preprocessing/index.jsx
+++ src/component/preprocessing/index.jsx
@@ -196,12 +196,18 @@ const Preprocessing = () => {
                             <div className='py-3 px-10 inline-block float-left'>{name + ':'}</div>
                             <div className='py-3 inline-block float-right'>
                                 <select {...input6} name={name+'_below'} className='py-2 px-5 focus:outline-none rounded-full' placeholder="Remove Above">
-                                    <option value="">-</option><option value="85%">85%</option><option value="90%">90%</option><option value="95%">95%</option>
+                                    <option value="">-</option>
+                                    <option value="55%">55%</option><option value="60%">60%</option><option value="65%">65%</option>
+                                    <option value="70%">70%</option><option value="75%">75%</option><option value="80%">80%</option>
+                                    <option value="85%">85%</option><option value="90%">90%</option><option value="95%">95%</option>
                                 </select>
                             </div>
                             <div className='py-3 inline-block float-right'>
                                 <select {...input6} name={name+'_above'} className='py-2 px-5 focus:outline-none rounded-full' placeholder="Remove Below">
                                     <option value="">-</option><option value="5%">5%</option><option value="10%">10%</option><option value="15%">15%</option>
+                                    <option value="20%">20%</option><option value="25%">25%</option><option value="30%">30%</option>
+                                    <option value="35%">35%</option><option value="40%">40%</option><option value="45%">45%</option>
+                                    <option value="50%">50%</option>
                                 </select>
                             </div>
                         </div>)}
